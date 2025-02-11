2f2c612e4d7e1229d6e9952119b92bafe8cc2b6f
alihossen4 <mdahossen.dev@gmail.com>
2025-02-11 17:20:51 +0600

    Update Home component to comment out NftCollection and adjust LatestBlog section styling

--- next-gen/src/pages/Home.jsx
+++ next-gen/src/pages/Home.jsx
@@ -11,7 +11,7 @@ const Home = () =>{
         <Brand />
         <NFT />
         <TopCreators />
-        <NftCollection />
+        {/* <NftCollection /> */}
         <NftColect />
     </>);
 };
--- next-gen/src/sections/LatestBlog.jsx
+++ next-gen/src/sections/LatestBlog.jsx
@@ -2,7 +2,7 @@ import Container from "../components/Container";
 
 const LatestBlog = ({className,children,img}) => {
   return (
-    <section className={`flex ${className}`}>
+    <section className={`flex ${className} `}>
             <div className={`border border-[#2E3150] rounded-xl bg-[#16192A] m-auto gap-[10px] ${className}`}>
                 <img src={img} alt="" />
                 {children}
