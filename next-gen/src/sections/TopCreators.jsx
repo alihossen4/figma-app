ce341d65d20be9685068e55b2b86ceb0805b049c
alihossen4 <mdahossen.dev@gmail.com>
2025-04-14 20:50:40 +0600

    make comment to this

--- next-gen/src/sections/TopCreators.jsx
+++ next-gen/src/sections/TopCreators.jsx
@@ -1,24 +1,24 @@
-import TopCreatorContainer from "../components/top-creators/TopCreatorContainer"
-import Title from "../components/Title"
-import Container from "../components/Container"
-import Button from "../components/Button"
-import Flex from "../components/Flex"
+// import TopCreatorContainer from "../components/top-creators/TopCreatorContainer"
+// import Title from "../components/Title"
+// import Container from "../components/Container"
+// import Button from "../components/Button"
+// import Flex from "../components/Flex"
 
-const TopCreators = () => {
-  return (
-    <section>
-        <Container >
-            <Flex className={`flex-col`}>
-            <Title title={"Top Creators"} className="text-center text-[#f2f2f2] font-black text-[56px] mb-6"/>
-            <p className="text-xl text-[#f7e3e3] max-w-[678px] text-center m-auto mb-5">In commodo auctor eget congue sit. Ultrices eget pretium sit euismod mi id. Risus, aliquam odio posuere ac in in nisl sed augue. </p>
+// const TopCreators = () => {
+//   return (
+//     <section>
+//         <Container >
+//             <Flex className={`flex-col`}>
+//             <Title title={"Top Creators"} className="text-center text-[#f2f2f2] font-black text-[56px] mb-6"/>
+//             <p className="text-xl text-[#f7e3e3] max-w-[678px] text-center m-auto mb-5">In commodo auctor eget congue sit. Ultrices eget pretium sit euismod mi id. Risus, aliquam odio posuere ac in in nisl sed augue. </p>
             
-            <Button text={'View all creators'} className='text-center self-center'/>
-            </Flex>
-            <TopCreatorContainer />
-        </Container>
+//             <Button text={'View all creators'} className='text-center self-center'/>
+//             </Flex>
+//             <TopCreatorContainer />
+//         </Container>
 
-    </section>
-  )
-}
+//     </section>
+//   )
+// }
 
-export default TopCreators
+// export default TopCreators
